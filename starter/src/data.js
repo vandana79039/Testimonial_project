2d2e86b72ecb21b70a0621d63a9b205529c50848
vandana79039 <vandana79039@gmail.com>
2025-10-06 15:19:39 +0530

    files added

---
+++ starter/src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Love Babbar",
+    job: "Founder of CodeHelp",
+    image: "https://avatars.githubusercontent.com/u/29489915?v=4",
+   
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Striver",
+    job: "Software Engineer at Google",
+    image: "https://avatars.githubusercontent.com/u/47192562?v=4",
+     text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Hitesh Chaudhary",
+    job: "Founder of Chai aur Code",
+    image: "https://avatars.githubusercontent.com/u/11613311?v=4",
+    text: "I am a retired corporate professional who has seamlessly transitioned into a full-time YouTuber. With a rich history as the founder of LCO (acquired) and a former CTO at iNeuron and Senior Director at PW, I bring a wealth of experience in building software and companies. ",
+  },
+  {
+    id: 4,
+    name: "Code With Harry",
+    job: "Content Creator",
+    image: "https://avatars.githubusercontent.com/u/48705673?v=4",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Aritra Biswas",
+    job: "Data Analyst",
+    image: "https://avatars.githubusercontent.com/u/93366359?v=4",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
